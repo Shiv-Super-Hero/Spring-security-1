90e6f32a14f829444063dbaaca61545a8ec6f679
Shiv-Super-Hero <shivnarayan1024@gmail.com>
2025-08-24 09:54:46 +0530

    message

--- src/main/java/com/shivaji/SpringSecurity1Application.java
+++ src/main/java/com/shivaji/SpringSecurity1Application.java
@@ -9,7 +9,7 @@ public class SpringSecurity1Application {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSecurity1Application.class, args);
 		int n = 10;
-
+		double jugaad = 45.6;
 		int j = 20;
 		String name = "John Snow";
 	}
